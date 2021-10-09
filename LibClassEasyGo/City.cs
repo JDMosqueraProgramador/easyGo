908a5e93904cb1d69d95ede5862152fa9735ed82
JDMosqueraProgramador <jdmm360@gmail.com>
2021-10-09 09:42:24 -0500

    Obtener ciudades

--- LibClassEasyGo/City.cs
+++ LibClassEasyGo/City.cs
@@ -6,13 +6,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-
+using Npgsql;
 namespace LibClassEasyGo
 {
     public class City
     {
         private int intIdCity;
         private string strNameCity;
+        private NpgsqlConnection conn = new Connect().Conn();
 
         public int IntIdCity { get => intIdCity; set => intIdCity = value; }
         public string StrNameCity { get => strNameCity; set => strNameCity = value; }
@@ -22,5 +23,33 @@ namespace LibClassEasyGo
             IntIdCity = intIdCity;
             StrNameCity = strNameCity;
         }
+
+        public City()
+        {
+
+        }
+
+        public List<City> GetCities()
+        {
+            List<City> cities = new List<City>();
+            string select = "SELECT intIdCity, strNameCity FROM tblCity";
+
+            NpgsqlCommand cmd = new NpgsqlCommand(select, conn);
+
+            NpgsqlDataReader data = cmd.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    cities.Add(new City((int)(data["intIdCity"]), data["strNameCity"].ToString()));
+                }
+            }
+
+            return cities;
+        }
+
     }
+
+
 }
