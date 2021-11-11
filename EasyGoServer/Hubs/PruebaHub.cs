47831a7b6806bc2969f443b29c0afb1366a8d9df
JDMosqueraProgramador <jdmm360@gmail.com>
2021-11-11 11:48:50 +0100

    Sever EasyGo

---
+++ EasyGoServer/Hubs/PruebaHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR; 
+
+
+namespace EasyGoServer.Hubs
+{
+    public class PruebaHub : Hub
+    {
+        public async Task SendState(string state)
+        {
+            await Clients.Others.SendAsync("ReceiveState", state);
+        }
+    }
+}
