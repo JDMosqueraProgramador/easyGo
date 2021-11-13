0b62e34ffb4b5ab38f3f50ce61b6717d352af079
JDMosqueraProgramador <jdmm360@gmail.com>
2021-11-13 22:06:28 +0100

    Controlador de viajes, envío de fecha de petición, ruta de detalles simplificada, calculo de precio

--- wEasyGoDriver/controllers/Travel.controller.cs
+++ wEasyGoDriver/controllers/Travel.controller.cs
@@ -4,20 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LibClassEasyGo;
-using Npgsql;
-
+using wEasyGoDriver.models;
 namespace wEasyGoDriver.controllers
 {
     class TravelController
     {
-        TravelController travel;
+         TravelModel travelModel;
 
         public TravelController()
         {
 
         }
 
+        public TravelController(string strStartingPlaceTravel, string strDestinationPlaceTravel, int intTotalPriceTravel, int numKMPriceTravel, DateTime dateRequestTravel, IUser customer, IMotorcycle moto)
+        {
+            travelModel = new TravelModel( strStartingPlaceTravel, strDestinationPlaceTravel, intTotalPriceTravel, numKMPriceTravel, dateRequestTravel, customer, moto);
+        }
 
+        public bool ExecuteCreateTravel()
+        {
+            return this.travelModel.InsertTravel();
+        }
+
+        public static int CalculePriceTravel(int distance)
+        {
+            return (distance * 200);
+        }
 
     }
 }
