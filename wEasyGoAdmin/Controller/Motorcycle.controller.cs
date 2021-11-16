6347cc8ba8432eb50ba45c836131d75a5dbc87e4
JDMosqueraProgramador <jdmm360@gmail.com>
2021-11-16 14:32:38 +0100

    Metodos para la obtención de datos en el administrador

---
+++ wEasyGoAdmin/Controller/Motorcycle.controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wEasyGoAdmin.Model;
+using LibClassEasyGo;
+
+namespace wEasyGoAdmin.Controller
+{
+    internal class MotorcycleController
+    {
+        MotorcycleModel motoModel;
+
+        public static IMotorcycle GetMoto(int idUser)
+        {
+            return new MotorcycleModel().SelectMoto(idUser);
+        }
+
+        public static Papers GetPapers(string licensePlate)
+        {
+            return new MotorcycleModel().SelectPapers(licensePlate);
+        }
+
+    }
+}
